a36fc40bf76456f628f6e000704f5154f70d4dcc
steven <stevenpyper12@gmail.com>
2019-03-22 11:17:32 +0000

    uploading base files

---
+++ nodev10/helloapp/routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Ciphers'});
+});
+router.get('/ciphers', function(req, res, next) {
+  res.render('index', { title: 'The ciphers Route' });
+});
+
+router.get('/ciphers/caesar', function(req, res, next) {
+  res.render('index', { title: 'The ceasar Route' });
+});
+
+
+
+router.get('/ciphers/substitution', function(req, res, next) {
+  res.render('index', { title: 'The substitution Route' });
+});
+
+
+
+router.get('/ciphers/morse', function(req, res, next) {
+  res.render('index', { title: 'The morse Route' });
+});
+
+
+
+module.exports = router;
